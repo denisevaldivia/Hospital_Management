cde8eb4eb22a3dae7da82b2d1e366349fde8daa5
LiliDYue <160811066+LiliDYue@users.noreply.github.com>
2025-11-28 21:42:19 -0600

    Create modelm.py
    Estructura del model para mongo by Lili

---
+++ modelm.py
@@ -0,0 +1,23 @@
+# ------------------------------------
+#   MONGODB
+# ------------------------------------
+
+# ------------------------------------
+#   QUERIES VACÍAS
+# ------------------------------------
+
+def query_1(db, user_input): pass
+def query_2(db, user_input): pass
+def query_3(db, user_input): pass
+def query_4(db, user_input): pass
+def query_5(db, user_input): pass
+def query_6(db, user_input): pass
+def query_7(db, user_input): pass
+def query_8(db, user_input): pass
+def query_9(db, user_input): pass
+def query_10(db, user_input): pass
+def query_11(db, user_input): pass
+def query_12(db, user_input): pass
+def query_13(db, user_input): pass
+def query_14(db, user_input): pass
+def query_15(db, user_input): pass
