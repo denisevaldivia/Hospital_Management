c2218f54d0da9943c1dd9f332455d1c670f336ed
LiliDYue <160811066+LiliDYue@users.noreply.github.com>
2025-11-28 21:42:59 -0600

    Create modeld.py
    Estructura para el model de Dgraph by Lili

---
+++ modeld.py
@@ -0,0 +1,139 @@
+# ------------------------------------
+#   DGRAPH MODELO
+# ------------------------------------
+import pydgraph
+
+def set_schema(client):
+    schema = """
+        # Primero declaramos los predicados
+        id_transaccion: int @index(int) .
+        id_paciente: int @index(int) .
+        id_sala: int @index(int) .
+        id_doctor: int @index(int) .
+        id_receta: int @index(int) .
+
+        nombre_servicio: string @index(term) .
+        precio: float @index(float) .
+
+        nombre: string .
+        sexo: string @index(hash) .
+        fecha_nacimiento: dateTime .
+        edad: int @index(int) .
+
+        tipo: string @index(term) .
+        especialidad: string @index(hash) .
+        licencia: string @index(hash) .
+        anios_experiencia: int @index(int) .
+        correo: string .
+        telefono: int .
+
+        nombre_visitante: string .
+        relacion_paciente: string @index(hash) .
+        motivo: string @index(term) .
+        hora_entrada: dateTime @index(hour) .
+        hora_salida: dateTime @index(hour) .
+
+        diagnostico: string @index(term) .
+        fecha_emision: dateTime @index(day) .
+        medicina: string @index(term) .
+        cantidad: int .
+        frecuencia: int .
+
+        GENERAN: uid @reverse .
+        SOLICITAN: [uid] @reverse .
+        RECIBEN: [uid] @reverse .
+        ATIENDEN: [uid] @reverse .
+        AGENDA: [uid] @reverse .
+        TIENEN: [uid] @reverse .
+        OTORGAN: [uid] @reverse .
+
+        # Ahora definimos los tipos
+        type Transaccion {
+            id_transaccion
+            nombre_servicio
+            precio
+            GENERAN
+        }
+
+        type Servicio {
+            id_paciente
+            nombre_servicio
+            GENERAN
+            SOLICITAN
+        }
+
+        type Paciente {
+            id_paciente
+            nombre
+            sexo
+            fecha_nacimiento
+            edad
+            ATIENDEN
+            SOLICITAN
+            RECIBEN
+        }
+
+        type Sala {
+            id_sala
+            id_doctor
+            tipo
+            AGENDA
+        }
+
+        type Doctor {
+            id_doctor
+            nombre
+            especialidad
+            licencia
+            anios_experiencia
+            correo
+            telefono
+            AGENDA
+            ATIENDEN
+            OTORGAN
+        }
+
+        type Visita {
+            id_paciente
+            nombre_visitante
+            relacion_paciente
+            motivo
+            hora_entrada
+            hora_salida
+            RECIBEN
+        }
+
+        type Receta {
+            id_receta
+            id_paciente
+            id_doctor
+            diagnostico
+            fecha_emision
+            medicina
+            cantidad
+            frecuencia
+            TIENEN
+            OTORGAN
+        }
+    """
+    op = pydgraph.Operation(schema=schema)
+    client.alter(op)
+    print("[DGRAPH] Schema configurado correctamente")
+
+# ------------------------------------
+#   QUERIES VACÍAS
+# ------------------------------------
+def query_1(client, user_input): pass
+def query_2(client, user_input): pass
+def query_3(client, user_input): pass
+def query_4(client, user_input): pass
+def query_5(client, user_input): pass
+def query_6(client, user_input): pass
+def query_7(client, user_input): pass
+def query_8(client, user_input): pass
+def query_9(client, user_input): pass
+def query_10(client, user_input): pass
+def query_11(client, user_input): pass
+def query_12(client, user_input): pass
+def query_13(client, user_input): pass
+def query_14(client, user_input): pass
